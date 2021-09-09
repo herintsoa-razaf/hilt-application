1c4a3c24ca0efc47a9a48a673569de71d659d5e0
Herintsoa Razafinandrianilana <herintsoa.razaf@backelite.com>
2021-09-09 17:32:26 +0200

    feat: show hello world page
    First commit

---
+++ app/src/main/java/com/example/hiltapplication/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.hiltapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/hiltapplication/MainFragment.kt
@@ -0,0 +1,41 @@
+package com.example.hiltapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.hiltapplication.databinding.FragmentMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainFragment : Fragment() {
+
+    private val viewModel: MainViewModel by viewModels()
+
+    private var viewBinding: FragmentMainBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return FragmentMainBinding.inflate(inflater, container, false).also {
+            viewBinding = it
+        }.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewBinding?.message?.text = viewModel.getMessage()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        viewBinding = null
+    }
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+}
